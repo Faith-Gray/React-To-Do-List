7a537cb998e4449dd6c986aec511db4d63a6939c
Faith <fhgray96@gmail.com>
2022-08-14 17:15:58 -0500

    add background color

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -5,7 +5,8 @@ const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
         <div>
             {toDoList.map(todo => {
                 return (
-                    <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>)
+                    <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>
+                )
             })}
            <button onClick={handleFilter}>Clear Completed</button>
             
